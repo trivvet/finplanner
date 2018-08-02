a7106890c6b5815297117a3b45ce2d557bcc2c77
Volodymyr Vlasiuk <trivvet@gmail.com>
2018-08-02 20:12:07 +0300

    add content for new home page

--- finance/views/finances.py
+++ finance/views/finances.py
@@ -3,5 +3,17 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from finance.models import Month, Account, Score
+
 def new_home(request):
-    return render(request, 'finance/new_home.html', {})
+    content = {}
+    if request.GET.get('list', '') == 'budgets':
+        content['months'] = Month.objects.exclude(approved=True)
+        content['add_form'] = 'budgets'
+    elif request.GET.get('list', '') == 'accounts':
+        content['accounts'] = Account.objects.all()
+    elif request.GET.get('list', '') == 'remnants':
+        content['remnants'] = Score.objects.all()
+    else:
+        content['months'] = Month.objects.filter(approved=True)
+    return render(request, 'finance/new_home.html', {'content': content})
